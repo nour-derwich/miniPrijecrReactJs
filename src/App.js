c433b6b79a3d98e8830c8027448cadf415e03e27
nour-logstic <88410368+nour-derwich@users.noreply.github.com>
2023-01-23 10:26:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {  Routes , Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Agents from "./compnonets/Agents";
+
 import Main from "./compnonets/Main";
 
 function App() {
